9e39624dd6f540aa8d5b522bc22f63b90c9e204c
KailanB <163462539+KailanB@users.noreply.github.com>
2026-03-01 19:40:28 -0500

    added diet type info and some additional values at later gens

--- dino-mutations.js
+++ dino-mutations.js
@@ -6,21 +6,24 @@ const MUTATIONS = [
         description: "Deal more damage to animals below 35% health.",
         value: "10%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["carnivore"]
     },
     {
         name: "Advanced Gestation (Female Only)",
         description: "Faster egg gestation/incubation/cooldown rate.",
         value: "50%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null
     },
     {
         name: "Barometric Sensitivity (Herbivore)",
         description: "Receive an indication prior to storms or droughts.",
         value: null,
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
 
     {
@@ -28,49 +31,56 @@ const MUTATIONS = [
         description: "Recovers health slightly faster.",
         value: "15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null
     },
     {
         name: "Congenital Hypoalgesia",
         description: "Reduce incoming damage when fighting larger species.",
         value: "15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null
     },
     {
         name: "Efficient Digestion",
         description: "Your food drains more slowly.",
         value: "20%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Enhanced Digestion",
         description: "Decrease nutrition decay rate.",
-        value: null,
+        value: "10%, 15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Enlarged Meniscus",
         description: "Fall damage hits stamina before draining health.",
         value: null,
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Epidermal Fibrosis",
         description: "Increase bleed resistance.",
-        value: "15%",
+        value: "15%, 20%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Featherweight",
         description: "Your footprints fade much faster.",
         value: "50%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Hematophagy (Carnivore)",
@@ -84,209 +94,239 @@ const MUTATIONS = [
         description: "Do extra damage on bleeding target.",
         value: "5%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["carnivore"]
     },
     {
         name: "Hydrodynamic",
         description: "Increased swimming speed.",
         value: "15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Hydro-regenerative",
         description: "Recover health faster during rainy weather.",
         value: "25%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Hypervigilance (Herbivore)",
         description: "Increases camera angles when eating and drinking. Increases footsteps audio from others.",
         value: "50%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
     {
         name: "Increased Inspiratory Capacity",
         description: "Increased O2 Capacity.",
         value: "15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Infrasound Communication",
         description: "Make significantly less noise when talking in chat.",
         value: "50%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Nocturnal",
         description: "Faster health/locked health recover and higher move speed at night.",
         value: "5%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Osteosclerosis",
         description: "Resist or Reduce fracture damage.",
         value: "20%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Photosynthetic Regeneration (Herbivore)",
         description: "Regenerate stamina faster during the day.",
         value: "10%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
     {
         name: "Photosynthetic Tissue",
         description: "Faster health/locked health recovery and higher move speed during the day.",
         value: "5%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Reabsorption",
         description: "Recover a small amount of water during the rainy weather or while swimming in drinkable water.",
-        value: "1",
+        value: "1, 2",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Social Behavior (Herbivore/Omnivore)",
         description: "Increased group size. Group Leader Only.",
         value: null,
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: [ "herbivore", "omnivore" ]
     },
     {
         name: "Submerged Optical Retention",
         description: "Increased underwater vision range.",
         value: "5%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Sustained Hydration",
         description: "Your water drains more slowly.",
         value: "20%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Truculency (Herbivore)",
         description: "Bucking has a higher chance to dismount latched animals.",
         value: "5%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
     {
         name: "Wader",
         description: "Less hindered when wading through shallow water.",
         value: "25%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Xerocole Adaptation (Herbivore)",
         description: "Gain some water when eating plants.",
         value: "15%",
         criteria: null,
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
     {
         name: "Gastronomic Regeneration",
         description: "Eating restores a small amount of health.",
         value: null,
         criteria: "slot 2–4",
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Hypermetabolic Inanition (Carnivore)",
         description: "The less hunger you have the more damage you deal.",
         value: null,
         criteria: "slot 2–4",
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["carnivore"]
     },
     {
         name: "Parthenogenesis",
         description: "Allows the player to reproduce without a mate.",
         value: null,
         criteria: "slot 2–4",
-        additionalInfo: "Does NOT transfer on Entomb"
+        additionalInfo: "Does NOT transfer on Entomb",
+        dietType: null,
     },
     {
         name: "Tactile Endurance (Herbivore)",
         description: "Convert incoming damage to stamina.",
-        value: null,
+        value: "?, 35%",
         criteria: "slot 2–4",
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: ["herbivore"]
     },
     {
         name: "Traumatic Thrombosis",
         description: "Prevent death from blood loss if resting.",
         value: null,
         criteria: "slot 2–4",
-        additionalInfo: null
+        additionalInfo: null,
+        dietType: null,
     },
     {
         name: "Augmented Tapetum (Carnivore)",
         description: "Increased vision at night.",
         value: null,
         criteria: "unlockable",
-        additionalInfo: "Kill 5 Players at night. Unlocks on Slot 2."
+        additionalInfo: "Kill 5 Players at night. Unlocks on Slot 2.",
+        dietType: ["carnivore"]
     },
     {
         name: "Cannibalistic (Carnivore)",
         description: "For species that are not, by default, cannibals. Adds their own species as a preferred prey for nutrients.",
         value: null,
         criteria: "unlockable",
-        additionalInfo: "Eat your own species. Unlocks on Slot 2. As of Update 0.18.11 the required amount needed to unlock this mutation has been reduced."
+        additionalInfo: "Eat your own species. Unlocks on Slot 2. As of Update 0.18.11 the required amount needed to unlock this mutation has been reduced.",
+        dietType: ["carnivore"]
     },
     {
         name: "Heightened Ghrelin",
         description: "Increased overeating capacity by a large amount.",
         value: null,
         criteria: "unlockable",
-        additionalInfo: "Maintain current hunger above 80% for 30 minutes. Unlocks on Slot 2."
+        additionalInfo: "Maintain current hunger above 80% for 30 minutes. Unlocks on Slot 2.",
+        dietType: null,
     },
     {
         name: "Multichambered Lungs",
-        description: "Stamina Pool: 100 - 1000 (Increased based on growth).",
-        value: null,
+        description: "Decrease stamina regeneration threshold",
+        value: "5%, 10%",
         criteria: "unlockable",
-        additionalInfo: "Drain 4500 stamina by sprinting or fast swimming. Unlocks on Slot 2/3."
+        additionalInfo: "Drain 4500 stamina by sprinting or fast swimming. Unlocks on Slot 2/3.",
+        dietType: null,
     },
     {
         name: "Osteophagic (Carnivore)",
         description: "Able to consume bones to regenerate fractures faster.",
         value: null,
         criteria: "unlockable",
-        additionalInfo: "Unlock by eating bones."
+        additionalInfo: "Unlock by eating bones.",
+        dietType: ["carnivore"]
     },
     {
         name: "Prolific Reproduction (Female Only)",
         description: "Your babies have increased health and stamina regen. Your babies require less food and they grow faster.",
-        value: null,
+        value: "10%, 15%",
         criteria: "unlockable",
-        additionalInfo: "Unlocks on Slot 2."
+        additionalInfo: "Unlocks on Slot 2.",
+        dietType: null,
     },
     {
         name: "Reinforced Tendons",
         description: "Jumping costs less stamina. Reduces take off stamina for Ptera.",
-        value: null,
+        value: "25%, 35%",
         criteria: "unlockable",
-        additionalInfo: "Unlock by jumping 50 times."
+        additionalInfo: "Unlock by jumping 50 times.",
+        dietType: null,
     },
     {
         name: "Reniculate Kidneys",
         description: "Can drink saltwater. Thirst Pool: 1000. Thirst intake value while drinking saltwater: -2.5%.",
         value: null,
         criteria: "unlockable",
-        additionalInfo: "Lose 1250 thirst by drinking saltwater. 50 thirst grants the 'Fluid Deficient' debuff and 1200 thirst removes it. Unlocks on Slot 2/3."
+        additionalInfo: "Lose 1250 thirst by drinking saltwater. 50 thirst grants the 'Fluid Deficient' debuff and 1200 thirst removes it. Unlocks on Slot 2/3.",
+        dietType: null,
     }
 ];
